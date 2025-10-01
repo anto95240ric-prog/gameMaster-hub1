545583fe71b73e60805e2adc794d2594945f7ae2
anto95240 <richard33antoine@gmail.com>
2025-10-01 12:04:14 +0200

    Add utilisation supabase dans main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,38 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
-  runApp(const GameMasterHubApp());
+import 'app.dart';
+import 'presentation/core/blocs/auth/auth_bloc.dart';
+// import 'presentation/core/blocs/theme/theme_bloc.dart';
+
+final supabase = Supabase.instance.client;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initialize Supabase
+  await dotenv.load(fileName: ".env");
+
+  // Initialiser Supabase avec les valeurs du .env
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL'] ?? '',
+    anonKey: dotenv.env['SUPABASE_ANON_KEY'] ?? '',
+  );
+  
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        // BlocProvider(create: (context) => ThemeBloc()),
+        BlocProvider(create: (context) => AuthBloc()),
+      ],
+      child: const GameMasterHubApp(),
+    ),
+  );
+
+  // 🔹 Test rapide
+  // final response = await supabase.from('joueur_sm').select().limit(1);
+  // print('Connexion test => $response');
+
 }

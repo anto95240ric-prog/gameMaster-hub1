5c69284ed5d26bd311f77a489c8b1b42bfbd11cb
anto95240ric-prog <anto95240ric@gmail.com>
2025-10-09 21:40:01 +0200

    Update joueur_sm_repository.dart

--- lib/domain/sm/repositories/joueur_sm_repository.dart
+++ lib/domain/sm/repositories/joueur_sm_repository.dart
@@ -1,8 +1,8 @@
 import '../entities/joueur_sm.dart';
 
 abstract class JoueurSmRepository {
-  Future<List<JoueurSm>> getAllJoueurs();
-  Future<JoueurSm> getJoueurById(int id);
+  Future<List<JoueurSm>> getAllJoueurs(int saveId);
+  Future<JoueurSm> getJoueurById(int id, int saveId);
   Future<void> insertJoueur(JoueurSm joueur);
   Future<void> updateJoueur(JoueurSm joueur);
   Future<void> deleteJoueur(int id);

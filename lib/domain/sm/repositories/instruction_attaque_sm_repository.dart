4c51baf837973fed52b9bd073c1e9f08b40a59ad
anto95240ric-prog <anto95240ric@gmail.com>
2025-10-09 21:38:56 +0200

    Update instruction_attaque_sm_repository.dart

--- lib/domain/sm/repositories/instruction_attaque_sm_repository.dart
+++ lib/domain/sm/repositories/instruction_attaque_sm_repository.dart
@@ -1,8 +1,8 @@
 import '../entities/instruction_attaque_sm.dart';
 
 abstract class InstructionAttaqueSmRepository {
-  Future<List<InstructionAttaqueSm>> getAllInstructions();
-  Future<InstructionAttaqueSm?> getInstructionByTactiqueId(int tactiqueId);
+  Future<List<InstructionAttaqueSm>> getAllInstructions(int saveId);
+  Future<InstructionAttaqueSm?> getInstructionByTactiqueId(int tactiqueId, int saveId);
   Future<void> insertInstruction(InstructionAttaqueSm instruction);
   Future<void> updateInstruction(InstructionAttaqueSm instruction);
   Future<void> deleteInstruction(int id);

7c52bf080b61ef358b1923a93280ec8165c16d1a
anto95240ric-prog <anto95240ric@gmail.com>
2025-10-09 21:28:22 +0200

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,10 @@
+// app.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'presentation/core/navigation/app_router.dart';
 import 'presentation/core/themes/app_theme.dart';
 import 'presentation/core/blocs/theme/theme_bloc.dart';
+import 'presentation/core/widgets/update_notify.dart';
 
 class GameMasterHubApp extends StatelessWidget {
   const GameMasterHubApp({super.key});
@@ -18,6 +20,11 @@ class GameMasterHubApp extends StatelessWidget {
           darkTheme: AppTheme.darkTheme,
           themeMode: state.isDarkMode ? ThemeMode.dark : ThemeMode.light,
           debugShowCheckedModeBanner: false,
+          builder: (context, child) {
+            return UpdateNotifier(
+              child: child ?? const SizedBox.shrink(),
+            );
+          },
         );
       },
     );

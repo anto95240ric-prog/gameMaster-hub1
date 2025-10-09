c1bba5632084afb3453511229efd871e3bf0ea1a
anto95240ric-prog <anto95240ric@gmail.com>
2025-10-09 21:39:27 +0200

    Update instruction_general_sm_repository.dart

--- lib/domain/sm/repositories/instruction_general_sm_repository.dart
+++ lib/domain/sm/repositories/instruction_general_sm_repository.dart
@@ -1,8 +1,8 @@
 import '../entities/instruction_general_sm.dart';
 
 abstract class InstructionGeneralSmRepository {
-  Future<List<InstructionGeneralSm>> getAllInstructions();
-  Future<InstructionGeneralSm?> getInstructionByTactiqueId(int tactiqueId);
+  Future<List<InstructionGeneralSm>> getAllInstructions(int saveId);
+  Future<InstructionGeneralSm?> getInstructionByTactiqueId(int tactiqueId, int saveId);
   Future<void> insertInstruction(InstructionGeneralSm instruction);
   Future<void> updateInstruction(InstructionGeneralSm instruction);
   Future<void> deleteInstruction(int id);

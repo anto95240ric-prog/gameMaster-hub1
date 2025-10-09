925c25de409f2cf1b45ef280f4fe42f338ed24fb
anto95240ric-prog <anto95240ric@gmail.com>
2025-10-09 21:39:12 +0200

    Update instruction_defense_sm_repository.dart

--- lib/domain/sm/repositories/instruction_defense_sm_repository.dart
+++ lib/domain/sm/repositories/instruction_defense_sm_repository.dart
@@ -1,8 +1,8 @@
 import '../entities/instruction_defense_sm.dart';
 
 abstract class InstructionDefenseSmRepository {
-  Future<List<InstructionDefenseSm>> getAllInstructions();
-  Future<InstructionDefenseSm?> getInstructionByTactiqueId(int tactiqueId);
+  Future<List<InstructionDefenseSm>> getAllInstructions(int saveId);
+  Future<InstructionDefenseSm?> getInstructionByTactiqueId(int tactiqueId, int saveId);
   Future<void> insertInstruction(InstructionDefenseSm instruction);
   Future<void> updateInstruction(InstructionDefenseSm instruction);
   Future<void> deleteInstruction(int id);
